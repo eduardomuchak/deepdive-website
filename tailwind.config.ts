bade901af5ff72a493f4bf2d727ea42a85bcea7e
Eduardo Muchak <eduardomuchak@gmail.com>
2023-09-02 15:17:05 -0300

    chore: :art: install and setup prettier to format the project code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: 'var(--font-roboto)',
-        alt: 'var(--font-raleway)',
+        sans: "var(--font-roboto)",
+        alt: "var(--font-raleway)",
+      },
+      colors: {
+        "brand-primary-blue": "#151f27",
+        "brand-secondary-blue": "#25313d",
+        "brand-orange": "#ff914d",
+        "brand-gray": "#b9c1be",
       },
     },
   },
