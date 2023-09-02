080db3483b707d413c7cbf1e5b8cc4fc40c65f4f
Eduardo Muchak <eduardomuchak@gmail.com>
2023-09-02 20:50:32 -0300

    fix: :art: adjusts header icons focus effect

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -32,25 +32,25 @@ function Header() {
               />
             </Link>
           </div>
-          <div className="-my-2 -mr-2 lg:hidden">
-            <Popover.Button
-              id="menu-button"
-              className="focus:ring-offset-background focus:primary-blue-500 hover:bg-primary-blue-500 flex cursor-pointer items-center rounded-xl px-2 py-2 transition-all focus:outline-none focus:ring-2 focus:ring-offset-2"
-              onMouseEnter={() => setIsIconHovering(true)}
-              onMouseLeave={() => setIsIconHovering(false)}
-            >
-              <List
-                size={24}
-                color={
-                  isIconHovering
-                    ? "rgba(255, 255, 255, 0.8)"
-                    : "rgba(255, 255, 255, 1)"
-                }
-                weight="bold"
-                aria-hidden="true"
-              />
-            </Popover.Button>
-          </div>
+
+          <Popover.Button
+            id="menu-button"
+            className="-my-2 -mr-2 flex items-center rounded-xl border-none bg-brand-primary-blue px-2 py-2 transition-all focus:outline-none focus:ring-2 focus:ring-brand-orange focus:ring-offset-0 lg:hidden"
+            onMouseEnter={() => setIsIconHovering(true)}
+            onMouseLeave={() => setIsIconHovering(false)}
+          >
+            <List
+              size={24}
+              color={
+                isIconHovering
+                  ? "rgba(255, 255, 255, 0.8)"
+                  : "rgba(255, 255, 255, 1)"
+              }
+              weight="bold"
+              aria-hidden="true"
+            />
+          </Popover.Button>
+
           <nav className="hidden items-center space-x-10 lg:flex">
             <Link href={"/"}>
               <span className="font-sans text-sm font-medium transition-all hover:opacity-80">
@@ -78,21 +78,26 @@ function Header() {
           className="absolute inset-x-0 top-0 h-screen origin-top-right transform rounded-b-xl bg-transparent p-0 transition lg:hidden"
         >
           <div className="mx-auto flex items-center justify-end px-6 py-5">
-            <div className="-my-2 -mr-2 lg:hidden">
-              <Popover.Button
-                id="close-menu-button"
-                className="focus:ring-offset-background focus:primary-blue-500 hover:bg-primary-blue-500 flex items-center rounded-xl bg-brand-primary-blue px-2 py-2 transition-all focus:outline-none focus:ring-2 focus:ring-offset-2"
-                onMouseEnter={() => setIsIconHovering(true)}
-                onMouseLeave={() => setIsIconHovering(false)}
-              >
-                <X
-                  size={24}
-                  color={"#FFFFFF"}
-                  weight="bold"
-                  aria-hidden="true"
-                />
-              </Popover.Button>
-            </div>
+            <Popover.Button
+              id="close-menu-button"
+              className="-my-2 -mr-2 flex items-center rounded-xl border-none bg-brand-primary-blue px-2 py-2 transition-all focus:outline-none focus:ring-2 focus:ring-brand-orange focus:ring-offset-0 lg:hidden"
+              onMouseEnter={() => setIsIconHovering(true)}
+              onMouseLeave={() => setIsIconHovering(false)}
+            >
+              <X
+                size={24}
+                color={
+                  isIconHovering
+                    ? "rgba(255, 255, 255, 0.8)"
+                    : "rgba(255, 255, 255, 1)"
+                }
+                weight="bold"
+                aria-hidden="true"
+                style={{
+                  outline: "none",
+                }}
+              />
+            </Popover.Button>
           </div>
           <nav className="flex h-full flex-col items-center justify-center bg-brand-primary-blue">
             <div className="flex grow-[5] items-center justify-center">
