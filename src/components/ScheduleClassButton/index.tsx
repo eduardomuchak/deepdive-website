e8b6100b8ebc3dd68054a21f255bef907a45437e
Eduardo Muchak <eduardomuchak@gmail.com>
2023-09-12 12:17:19 -0300

    fix: :bug: translate the schedule experimental class button

--- src/components/ScheduleClassButton/index.tsx
+++ src/components/ScheduleClassButton/index.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { Button } from "../_Primitives/Button";
 
 export function ScheduleClassButton() {
+  const t = useTranslations("ScheduleClass");
+
   return (
     <div className="sticky bottom-0 left-0 z-50 flex h-[84px] items-center justify-center bg-brand-primary-blue sm:hidden">
       <Link
@@ -9,7 +13,7 @@ export function ScheduleClassButton() {
         href={"https://deepdiveschool.rds.land/diagnostico"}
         target="_blank"
       >
-        <Button className="max-w-[227px]">Agendar aula experimental</Button>
+        <Button className="max-w-[250px]">{t("button")}</Button>
       </Link>
     </div>
   );
